eaf34d7212402c74e9f3313df19fa64c44b108d3
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-03-12 00:58:29 -0300

    Module 02-aula 04

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Botao from './components/Botao';
+import Formulario from './components/Formulario';
 
 function App() {
-	return (
-		<div className='App'>
-			<Botao />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Formulario />
+    </div>
+  );
 }
 
 export default App;
